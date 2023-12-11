9f1b1f3cc93e69698714686d51b1c450970e3f47
gayashan <gayashan@ebipl.com>
2023-12-11 12:05:13 +0530

    test

--- api/index.py
+++ api/index.py
@@ -1,37 +1,27 @@
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-# from openai import OpenAI
-
-# app = Flask(__name__)
-# CORS(app)
-
-# # Set your API key here
-# api_key = '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'
-# client = OpenAI(api_key=api_key)
-
-# def get_response(message):
-#     chat_completion = client.chat.completions.create(
-#         messages=[{"role": "user", "content": message}],
-#         model="gpt-4-1106-preview",
-#     )
-#     return chat_completion.choices[0].message.content
-
-# @app.route('/chat', methods=['POST'])
-# def chat():
-#     data = request.json
-#     message = data['message']
-#     response = get_response(message)
-#     return jsonify({"response": response})
-
-
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from openai import OpenAI
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Set your API key here
+api_key = 'your-api-key'
+client = OpenAI(api_key=api_key)
 
-@app.route('/about')
-def about():
-    return 'About'
+def get_response(message):
+    chat_completion = client.chat.completions.create(
+        messages=[{"role": "user", "content": message}],
+        model="gpt-4-1106-preview",
+    )
+    return chat_completion.choices[0].message.content
+
+@app.route('/chat', methods=['GET'])
+def chat():
+    message = request.args.get('message')  # Get message from query string
+    if message:
+        response = get_response(message)
+        return jsonify({"response": response})
+    else:
+        return jsonify({"error": "No message provided"}), 400
+
