6dbff2ddcba48fef39ffaa1984a28c73e4384948
gayashan <gayashan@ebipl.com>
2023-12-11 12:17:05 +0530

    test

--- api/index.py
+++ api/index.py
@@ -6,16 +6,15 @@ app = Flask(__name__)
 CORS(app)
 
 # Set your API key here
-api_key = 'your-api-key'
+api_key = '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'
 client = OpenAI(api_key=api_key)
 
 def get_response(message):
-    return message
-    # chat_completion = client.chat.completions.create(
-    #     messages=[{"role": "user", "content": message}],
-    #     model="gpt-4-1106-preview",
-    # )
-    # return chat_completion.choices[0].message.content
+    chat_completion = client.chat.completions.create(
+        messages=[{"role": "user", "content": message}],
+        model="gpt-4-1106-preview",
+    )
+    return chat_completion.choices[0].message.content
 
 @app.route('/chat', methods=['GET'])
 def chat():
